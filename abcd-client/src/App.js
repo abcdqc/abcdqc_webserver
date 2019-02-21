239aaa05b684d6be40fdcf7ec6b0481b67b51615
Georgi Ivanov <georgi.ivanov@nih.gov>
2019-02-21 11:15:48 -0500

    d3

--- abcd-client/src/App.js
+++ abcd-client/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
+import Dashboard from './Dashboard';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-	<NavBar />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <NavBar/>
+                <Dashboard/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ abcd-client/src/Dashboard.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import { scaleBand, scaleLinear } from 'd3-scale';
+import { tsvParse } from 'd3-dsv';
+import { max } from 'd3-array';
+import { axisBottom, axisLeft } from 'd3-axis';
+import { select } from 'd3-selection';
+
+import dataTsv from './data';
+
+const svgWidth = 960,
+    svgHeight = 500;
+//Note: getting width and height from a variable rather than the elements attribute e.g. svg.attr("width")
+const margin = { top: 20, right: 20, bottom: 30, left: 40 },
+    width = svgWidth - margin.left - margin.right,
+    height = svgHeight - margin.top - margin.bottom;
+const x = scaleBand()
+        .rangeRound([0, width])
+        .padding(0.1),
+    y = scaleLinear().rangeRound([height, 0]);
+const data = tsvParse(dataTsv, d => {
+    d.frequency = +d.frequency;
+    return d;
+});
+x.domain(data.map(d => d.letter));
+y.domain([0, max(data, d => d.frequency)]);
+
+class Dashboard extends Component {
+    state = {
+        ready: true,
+        searchString: '',
+        ages: ['0-17', '18-54', '55+']
+    };
+
+    onSearchInputChange = (event) => {
+        if (event.target.value) {
+            this.setState({searchString: event.target.value})
+        } else {
+            this.setState({searchString: ''})
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                { this.state.ready ? (
+                <div>
+                    <Grid container spacing={24} style={{padding: 24}}>
+                        {/*{ this.state.ages.map(age => (*/}
+                        <Grid item xs={12} sm={6} lg={4} xl={3}>
+                            <TextField style={{padding: 24}}
+                                       id="searchInput"
+                                       placeholder="Search for ___"
+                                       margin="normal"
+                                       onChange={this.onSearchInputChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6} lg={8} xl={9}>
+                            <svg width={500} height={500}>
+                                <g transform={`translate(${margin.left}, ${margin.top})`}>
+                                    <g
+                                        className="axis axis--x"
+                                        transform={`translate(0, ${height})`}
+                                        ref={node => select(node).call(axisBottom(x))}
+                                    />
+                                    <g className="axis axis--y">
+                                        <g ref={node => select(node).call(axisLeft(y).ticks(10, '%'))} />
+                                        {/* Note: In the actual example 'Frequency' is a child of the above 'g' and it doesn't render.
+          * Changing it to a sibiling allows it to render and having the axis as an empty 'g' means that it will also play nicer with react:
+          * "The easiest way to avoid conflicts is to prevent the React component from updating. You can do this by rendering elements that React has no reason to update, like an empty <div />."
+          * https://reactjs.org/docs/integrating-with-other-libraries.html
+          */}
+                                        <text transform="rotate(-90)" y="6" dy="0.71em" textAnchor="end">
+                                            Frequency
+                                        </text>
+                                    </g>
+                                    {data.map(d => (
+                                        <rect
+                                            key={d.letter}
+                                            className="bar"
+                                            x={x(d.letter)}
+                                            y={y(d.frequency)}
+                                            width={x.bandwidth()}
+                                            height={height - y(d.frequency)}
+                                        />
+                                    ))}
+                                </g>
+                            </svg>
+                        </Grid>
+                        {/*//))}*/}
+                    </Grid>
+                </div>
+                ) : "No state found" }
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
